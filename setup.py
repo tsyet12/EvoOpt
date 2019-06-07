7aa81e767269d46303e06e1668b80e6a7552b34a
tsyet12 <tsyet12@gmail.com>
2019-06-07 16:38:46 +0200

    release

--- setup.py
+++ setup.py
@@ -1,12 +1,21 @@
 from setuptools import setup, find_packages
 
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+
+
 setup(name='EvoOpt', 
-version='0.1', 
+version='0.12', 
 license='BSD 2-Clause',
 description="EvoOpt: Python Implementation of State-of-Art Evolutionary Algorithms",
 author='Sin Yong Teng',
+long_description=long_description,
+long_description_content_type="text/markdown",
 author_email='tsyet12@gmail.com',
-download_url = 'https://github.com/tsyet12/EvoOpt',
 keywords = ['Evolutionary', 'Optimization', 'Algorithm'],
 packages=find_packages(),
 setup_requires=['numpy', 'matplotlib'],
@@ -14,7 +23,7 @@ classifiers=[
     'Development Status :: 3 - Alpha',      
     'Intended Audience :: Developers',      # Define that your audience are developers
     'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: BSD 2-Clause',   # Again, pick a license
+    'License :: OSI Approved :: BSD License',   # Again, pick a license
     'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
     'Programming Language :: Python :: 3.4',
     'Programming Language :: Python :: 3.5',
